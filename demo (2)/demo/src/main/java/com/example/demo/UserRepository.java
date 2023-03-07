d9cd5c099239ec5374236e279fe54ef30b88eb49
siwookoon <https://github.com/siwookoon>
2023-03-07 12:47:36 +0900

    updated

--- demo (2)/demo/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
